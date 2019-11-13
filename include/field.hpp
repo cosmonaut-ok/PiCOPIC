a8e7c116858b612dadeab511fad6de8b4dcc342e
Oleksandr Vynnyk <oleksandr.v@btigroup.io>
2019-11-13 14:00:10 +0200

    update default config

--- include/field.hpp
+++ include/field.hpp
@@ -19,6 +19,16 @@ public:
   Field(void) {};
   Field(Geometry *geom, TimeSim *t) : geometry(geom), time(t)
   {
+    //! The important feature is to leave so called 'overlay areas'
+    //! around field and current grids. In this case it takes
+    //! one grid cell before and after main grid area.
+    //! e.g. overlay areas are grid[0:1][i], grid[i][0:1],
+    //! grid[0:1][geometry->z_grid_amount-1:geometry->z_grid_amount-2]
+    //! and grid[geometry->r_grid_amount-1:geometry->r_grid_amount-2][i]
+    //! So, we need to shift all of the grid numbers to 2
+    //! and count it from 1 to geometry->(r,z)_grid_amount
+    //! instead of 0 to geometry->(r,z)_grid_amount
+
     field = Grid3D<double> (geometry->r_grid_amount + 2, geometry->z_grid_amount + 2);
     time = t;
     field = 0;
